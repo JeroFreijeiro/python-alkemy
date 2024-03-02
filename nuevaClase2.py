5acaf0fb1e12b91eedd1e664484ce0f0396bdb07
JeroFreijeiro <jerofreijeiro@gmail.com>
2024-03-02 20:50:54 -0300

    class nº2 se trabajo sobre tipos de datos, operadores, variables y estructuras de control-condicionales

--- nuevaClase2.py
+++ nuevaClase2.py
@@ -1,3 +1,26 @@
+# Matinee entre 13 y 17 años
+# 1. Edad tiene que ser mayor o igual a 13
+# 2. Edad tiene que ser menor o igual a 17 
+
+edad = 21
+
+print((edad >= 13) and (edad <= 17))
+
+# AND - CONJUNCIÓN
+# V + V = V
+# V + F = F
+# F + V = F
+# F + F = F
+
+# OR - DISYUNCIÓN
+# V + V = V
+# V + F = V
+# F + V = V
+# F + F = F
+
+
+
+
 # Tipos de datos
 
 # Numérico - int (número entero, sin comas)
@@ -35,3 +58,37 @@ print(div)
 
 raizCuadrada = num1 ** num2
 print(raizCuadrada)
+
+
+# ASIGNACIÓN = ESTA VARIABLE ES (ESTE) DATO
+variable = "Hola" # Esta variabe es el string "Hola"
+
+print(variable)
+
+# COMPARACIÓN = ESTA VARIABLE (ES IGUAL A) ESTA OTRA VARIABLE - devuelve un boolean
+print(variable == "Hola") #true
+print(variable == "hola") #false
+
+# DISTINTO QUE (ES LO OPUESTO A "IGUAL QUE")
+
+print(variable != "hola") #true
+
+
+edad = 18
+
+print(edad == 18) # igual que (18)  
+print(edad != 18) # distinto que (18)
+print(edad > 18) # mayor que (19, 20, 21, ...)  
+print(edad >= 18) # mayor o igual que (18, 19, 20, 21, ...)    
+print(edad < 18) # menor que (16, 17, 18, ...)  
+print(edad <= 18) # menor o igual que (16, 17, 18, ...)
+
+
+edad = 14
+print(edad)
+edad = edad + 1 #Puede simplificarse en el ejemplo de abajo (+=)
+print(edad)
+edad += 1
+print(edad)
+edad -= 1
+print(edad)
--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,37 @@
 print("Soy el nuevo archivo Python")
+
+# En Python si quiero crear una CONSTANTES debe inicializarse con mayúscula
+# Las VARIABLES no se inician con mayúscula
+# Python SI es case sensitive
+
+nombre = "Juan"
+apellido = "Perez"
+edad = 21
+
+nombre_entero = nombre + " " + apellido
+
+saludo = f'Hola! Mi nombre es {nombre_entero} y tengo {edad} anios.'
+
+print(saludo)
+
+# ESTRUCTURAS DE CONTROL
+# Con estructuras de control creamos reglas y escenarios para tomar decisiones en el orden del flujo y en base a los resultados que aparecen
+
+# ESTRUCTURAS DE CONTROL Nº 1  - CONDICIONALES IF / ELIF / ELSE
+
+# Boliche 18 o más años
+# Matinee entre 13 y 17
+
+edad = 18
+esSuCumpleanios = True
+
+if (edad >= 18):
+    print(f'Tenes {edad} anios, podes entrar al boliche.')
+    if(esSuCumpleanios):
+        print(f'Feliz cumple, te ganaste un trago')
+elif((edad >=13) and (edad <=17)):
+    print(f'Tenes {edad} anios, podes entrar a la matinee.')
+    if(esSuCumpleanios):
+        print(f'Feliz cumple, te ganaste una gaseosa')
+else:
+    print(f'Tenes {edad} anios, no podes entrar.')
