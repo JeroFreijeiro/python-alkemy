2d43c7b5635eb49c540b34a09562aaf1bdfadc0c
JeroFreijeiro <jerofreijeiro@gmail.com>
2024-03-01 23:57:19 -0300

    3rd commit

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 suma = 20+5
 print("Hola mundo")
 print(suma)
+
+""" another example """
+
+print("Hello world!")
+
+saludo = "Hola mundo!"
+print(saludo)
+
+def saludar():
+    #uso de la identación que indica 
+    #que esta línea esta dentro de la sentencia previa
+    return "Hello world!"
+
+print(saludar())
