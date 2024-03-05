b81164e4d9c2c915308d7f0e3344e78e53637ae2
JeroFreijeiro <jerofreijeiro@gmail.com>
2024-03-05 00:06:30 -0300

    3rd class 5th commit funciones + algoritmos

--- funcionesClase3.py
+++ funcionesClase3.py
@@ -45,3 +45,57 @@ sumar_iva(496)
     1996.5
     1028.5
     600.16 """   
+
+
+# ALGORITMOS
+# 1. ENTRADA DE DATOS - en la función serían los parámetros
+# 2. PROCESAMIENTO DE DATOS
+# 3. RETORNO DE DATOS
+
+def sumar_iva(precio): # Declaramos la función - 1. ENTRADA DE DATOS
+    return precio * 1.21 # Definir la función - 2. PROCESAMIENTO DE DATOS
+
+print(sumar_iva(100)) # invocar / llamar - 3. RETORNO DE DATOS
+print(sumar_iva(2960))
+print(sumar_iva(1650))
+print(sumar_iva(850))
+print(sumar_iva(496))
+
+""" 121.0
+    3581.6
+    1996.5
+    1028.5
+    600.16 """
+
+def sumar_iva(precio, porcIva): 
+    resultado = precio * porcIva
+    return resultado 
+
+print(sumar_iva(100, 1.10)) 
+print(sumar_iva(2960, 1.18))
+print(sumar_iva(1650, 1.21))
+print(sumar_iva(850, 1.25))
+print(sumar_iva(496, 1.20))
+
+""" 10.00000000000001
+    3492.7999999999997
+    1996.5
+    1062.5
+    595.1999999999999 """
+
+def sumar_iva(nombre,precio, porcIva): 
+    resultado = precio * porcIva
+    saludo = f'Hola {nombre} precio total {resultado}'
+    return saludo 
+
+print(sumar_iva("Juan",100, 1.10)) 
+print(sumar_iva("Pedro",2960, 1.18))
+print(sumar_iva("Mario",1650, 1.21))
+print(sumar_iva("Jose",850, 1.25))
+print(sumar_iva("Hernan",496, 1.20))
+
+""" Hola Juan precio total 110.00000000000001
+    Hola Pedro precio total 3492.7999999999997
+    Hola Mario precio total 1996.5
+    Hola Jose precio total 1062.5
+    Hola Hernan precio total 595.1999999999999 """
