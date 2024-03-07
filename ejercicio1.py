31441fa2a035bb89a1d6ad07ad3d0441ae179b11
JeroFreijeiro <jerofreijeiro@gmail.com>
2024-03-06 21:15:52 -0300

    3rd class 6th commit creación archivo ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+""" CONSIGNA
+Crear un pequeño programa que realice una función aritmética que permita al usuario ingresar datos
+por la terminal acorde a distintas opciones. Para ello debemos definir una función que reciba
+parámetros:
+
+- Combinar funciones nativas y funciones definidas, condicionales y bucles.
+- Si el usuario ingresa el nro 1, realiza la acción A.
+- Si el usuario ingresa el nro 2, realiza la acción B.
+"""
+
