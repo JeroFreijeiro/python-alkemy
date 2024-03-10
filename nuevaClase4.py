60b3fd73b65d3b2bdd02ef5b1a21a86d81ededf9
JeroFreijeiro <jerofreijeiro@gmail.com>
2024-03-09 21:41:09 -0300

    4th class 1st commit primera parte explicación POO

---
+++ nuevaClase4.py
@@ -0,0 +1,35 @@
+# REPASO
+# - Funciones nativas de Python
+# - Funciones personalizadas
+# - Funciones parametrizadas y su ejecución
+
+# POO es un paradigma más, de entre varios
+
+# OTROS TIPOS DE PARADIGMAS
+""" 
+    1- Paradigma imperativo
+    2- Paradigma funcional
+    3- Paradigma declarativo
+    4- Paradigma reactivo
+"""
+
+# POO permite manipular los objetos, de acuerdo a 3 propiedades:
+# 1- las propiedades
+# 2- el comportamiento
+# 3- el estado
+
+# POO - Un objeto es una entidad 
+# ATRIBUTOS/PROPIEDADES - que tiene determinadas características
+# MÉTODOS - puede realizar acciones, tiene 'x' comportamiento
+# ESTADOS - puede tener un estado
+
+
+# CLASES
+
+# Un OBJETO es una INSTANCIA de una CLASE
+# ejemplo en clase: "los objetos que hoy vamos a estar creando van a tener una estructura (una especie
+# de molde), que es la CLASE, una vez que determinemos que es lo que va a componer esa CLASE AUTO,
+# vamos a crear 1, 2, 10 autos, y cada uno de esos autos que creemos es una INSTANCIA DE ESA CLASE.
+# O sea, cada auto que creemos es un OBJETO, y cada objeto que creemos es una INSTANCIA DE ESA CLASE (AUTO)"
+
+
